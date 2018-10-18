jrallison/go-workers

Switch to `go-redis` driver, add go.mod to manage project dependencies, and increment go version dependency to 1.11.

go.mod
@@ -1,0 +1,11 @@
+module github.com/stefannegrea/go-workers
+
+require (
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/customerio/gospec v0.0.0-20130710230057-a5cc0e48aa39
+	github.com/go-redis/redis v6.14.1+incompatible
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/onsi/gomega v1.4.2 // indirect
+	github.com/orfjackal/nanospec.go v0.0.0-20120727230329-de4694c1d701 // indirect
+)

digitalocean/go-workers2

go.mod
@@ -1,0 +1,11 @@
+module github.com/digitalocean/go-workers2
+
+require (
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/customerio/gospec v0.0.0-20130710230057-a5cc0e48aa39
+	github.com/go-redis/redis v6.14.1+incompatible
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/onsi/gomega v1.4.2 // indirect
+	github.com/orfjackal/nanospec.go v0.0.0-20120727230329-de4694c1d701 // indirect
+)