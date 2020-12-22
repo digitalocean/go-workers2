04070bb1d7b7e82bd94762e7febb0f2566bc3daf
Stefan Negrea <3912943+stefannegrea@users.noreply.github.com>
2020-12-22 09:43:24 -0600

    Small refactoring and add missing comments.

--- msg.go
+++ msg.go
@@ -1,14 +1,16 @@
 package workers
 
 import (
-	"github.com/bitly/go-simplejson"
 	"reflect"
+
+	"github.com/bitly/go-simplejson"
 )
 
 type data struct {
 	*simplejson.Json
 }
 
+// Msg is the struct for job data (parameters and metadata)
 type Msg struct {
 	*data
 	original  string
@@ -16,31 +18,37 @@ type Msg struct {
 	startedAt int64
 }
 
+// Args is the set of parameters for a message
 type Args struct {
 	*data
 }
 
+// Class returns class attribute of a message
 func (m *Msg) Class() string {
 	return m.Get("class").MustString()
 }
 
+// Jid returns job id attribute of a message
 func (m *Msg) Jid() string {
 	return m.Get("jid").MustString()
 }
 
+// Args returns arguments attribute of a message
 func (m *Msg) Args() *Args {
 	if args, ok := m.CheckGet("args"); ok {
 		return &Args{&data{args}}
-	} else {
-		d, _ := newData("[]")
-		return &Args{d}
 	}
+
+	d, _ := newData("[]")
+	return &Args{d}
 }
 
+// OriginalJson returns the original JSON message
 func (m *Msg) OriginalJson() string {
 	return m.original
 }
 
+// ToJson return data in JSON format th message
 func (d *data) ToJson() string {
 	json, err := d.Encode()
 
@@ -52,10 +60,11 @@ func (d *data) ToJson() string {
 }
 
 func (d *data) Equals(other interface{}) bool {
-	otherJson := reflect.ValueOf(other).MethodByName("ToJson").Call([]reflect.Value{})
-	return d.ToJson() == otherJson[0].String()
+	otherJSON := reflect.ValueOf(other).MethodByName("ToJson").Call([]reflect.Value{})
+	return d.ToJson() == otherJSON[0].String()
 }
 
+// NewMsg returns a new message
 func NewMsg(content string) (*Msg, error) {
 	d, err := newData(content)
 	if err != nil {
