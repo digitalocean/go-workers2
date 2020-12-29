50c3b22f57c52e9088f8444b66e09cc081ab69dc
Tom Czarniecki <tomcz@users.noreply.github.com>
2020-12-29 15:51:24 +1000

    allow users to use their own API server

--- api_server.go
+++ api_server.go
@@ -30,12 +30,17 @@ var globalAPIServer = &apiServer{
 	managers: map[string]*Manager{},
 }
 
-// StartAPIServer starts the API server
-func StartAPIServer(port int) {
+// GlobalAPIHandler exports the API handler
+func GlobalAPIHandler() http.Handler {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/stats", globalAPIServer.Stats)
 	mux.HandleFunc("/retries", globalAPIServer.Retries)
+	return mux
+}
+
+// StartAPIServer starts the API server
+func StartAPIServer(port int) {
+	mux := GlobalAPIHandler()
 
 	Logger.Println("APIs are available at", fmt.Sprintf("http://localhost:%v/", port))
 
